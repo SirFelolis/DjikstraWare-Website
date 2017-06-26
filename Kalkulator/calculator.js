b7b726b91910d0770351a311aa8f180b562bdfb0
Oliver Thunæs <Felolis30@gmail.com>
2017-06-26 18:32:31 +0200

    Added calculator page

---
+++ Kalkulator/calculator.js
@@ -0,0 +1,22 @@
+
+
+// Number of people selling
+var numPeople = document.getElementById("numPeople");
+
+// Number of items sold
+var numSocks = document.getElementById("numSocks");
+var numToiletpaper = document.getElementById("numToiletpaper");
+var numFireblankets = document.getElementById("numFireblankets")
+var numMultipro = document.getElementById("numMultipro");
+var numOriginal = document.getElementById("numOriginal");
+var numWristbrands = document.getElementById("numWristbands");
+var numScreenprot = document.getElementById("numScreenprotectors");
+
+var result = document.getElementById("result");
+
+// The value in kr of each item
+
+function CalculateRevenue()
+{
+    
+}
